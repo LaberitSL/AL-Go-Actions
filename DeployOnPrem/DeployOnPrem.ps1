50585dc4cd2dd872f55b732d79f11649d78e0771
Alberto Sáez Salinas <asaez@laberit.com>
2023-03-31 10:09:26 +0200

    Fix unpublish onpremdeploy

--- DeployOnPrem/DeployOnPrem.ps1
+++ DeployOnPrem/DeployOnPrem.ps1
@@ -165,7 +165,8 @@ try {
                 $sync = $true
                 $install = $true
                 $upgrade = $true
-                $unPublishOldVersion = $true
+                #$unPublishOldVersion = $true
+                $unPublishOldVersion = $false
                 
                 $publishArgs = @{ "packageType" = $packageType }
                 if ($scope) {
